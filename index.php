f9aab44fa8e42331d9742d8b9920a8af2d169004
SAYri <34859317+SAYri-ua@users.noreply.github.com>
2019-03-01 02:06:43 +0200

    fix
    Fixed index.php and services.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	</div>
 	
 	<div class="section-white section-icons">
-		<div class="uk-container uk-container-center container-940">
+		<div class="uk-container uk-container-center container-940 padding-LR-48">
 			<div class="slideshow-promo slideshow-promo_pulltop">
 				<div class="uk-slidenav-position" data-uk-slideshow>
 					<ul class="uk-slideshow">
@@ -26,8 +26,8 @@
 							<div class="slideshow-promo-slide">
 								<div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
 								<div class="slide-content">
-									<div class="slide-title">Заголовок акции, сезонного уникального предложения</div>
-									<div class="slide-text">Дата проведения акции 1- 9 января</div>
+                                    <div class="slide-title text-stroke">Скидка 33%</div>
+                                    <div class="slide-text">в будние дни до 15:00</div>
 									<a class="uk-button slide-button">Подробнее</a>
 								</div>
 							</div>
@@ -36,8 +36,8 @@
 							<div class="slideshow-promo-slide">
 								<div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
 								<div class="slide-content">
-									<div class="slide-title">Заголовок акции, сезонного уникального предложения</div>
-									<div class="slide-text">Дата проведения акции 1- 9 января</div>
+									<div class="slide-title uk-h1 text-stroke">Скидка 23%</div>
+									<div class="slide-text">в будние дни до 15:00</div>
 									<a class="uk-button slide-button">Подробнее</a>
 								</div>
 							</div>
@@ -51,21 +51,23 @@
 					</ul>
 				</div>
 			</div>
-			<div class="uk-h2 uk-margin-large-bottom text-center-small text-22-small">
-				Игры-приключения в стиле «Форт Боярд» <br>
-				У нас никто не проигрывает — каждый выходит победителем!
+			<div class="text-center-small text-22-small">
+                <p class="services-blocks-title">Центр «Галактика Приключений»</p>
+                <p class="services-blocks-txt">Это уникальный проект для участников от 6 лет, где команды до 36 человек проходят игры-приключения,
+                    решают головоломки и становятся настоящими Хранителями Галактики, открывая в себе новые невероятные
+                    суперспособности.</p>
 			</div>
-			<div class="services-blocks uk-grid uk-grid-small uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-1-1 uk-margin-large-bottom" data-uk-grid-match="{target:'.service-teaser'}" data-uk-grid-margin>
+			<div class="services-blocks-padding services-blocks uk-grid uk-grid-small uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-1-1 uk-margin-large-bottom" data-uk-grid-match="{target:'.service-teaser'}" data-uk-grid-margin>
 				<div>
 					<div class="service-teaser service-birthday">
-						<div class="service-icon"><img src="images/icon-birthday.svg" alt=""></div>
+<!--						<div class="service-icon"><img src="images/icon-birthday.svg" alt=""></div>-->
 						<div class="service-name">Дни рождения</div>
 						<a href="" class="uk-position-cover"></a>
 					</div>
 				</div>
 				<div>
 					<div class="service-teaser service-group">
-						<div class="service-icon"><img src="images/icon-group.svg" alt=""></div>
+<!--						<div class="service-icon"><img src="images/icon-group.svg" alt=""></div>-->
 						<div class="service-name">Групповые
 						посещения</div>
 						<a href="" class="uk-position-cover"></a>
@@ -74,7 +76,7 @@
 				</div>
 				<div>
 					<div class="service-teaser service-individual">
-						<div class="service-icon"><img src="images/icon-individual.svg" alt=""></div>
+<!--						<div class="service-icon"><img src="images/icon-individual.svg" alt=""></div>-->
 						<div class="service-name">Индивидуальное посещение</div>
 						<a href="" class="uk-position-cover"></a>
 
@@ -88,44 +90,65 @@
 	</div>
 	<div class="section-like">
 		<div class="uk-container uk-container-center container-940" data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
-			<div class="uk-h1 text-stroke uk-text-center uk-margin-large-bottom">
-				Вам у нас понравится
-			</div>
-			<div class="like-list uk-grid uk-grid-width-medium-1-2" data-uk-grid-margin data-uk-grid-match>
+			<div class="uk-h1 text-stroke uk-text-center">
+                Проведение праздника
+            </div>
+            <div class="uk-text-center section-like__slogan">
+                в нашем центре – это
+            </div>
+			<div class="like-list like-list-index uk-grid uk-grid-width-medium-1-2" data-uk-grid-margin data-uk-grid-match>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-1.svg" alt=""></div>
-						<div class="like-text">Увлекательное приключение по мотивам известной игры «Форт Боярд»</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-1.jpg" alt=""></div>
+						<div class="like-text">
+                            <p class="text-stroke">01</p>
+                            10 интерактивных площадок с прохождением испытаний разной сложности для участников от 6 лет
+                        </div>
 					</div>
 				</div>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-2.svg" alt=""></div>
-						<div class="like-text">Максимальное погружение участника в атмосферу настоящего приключения</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-2.jpg" alt=""></div>
+						<div class="like-text">
+                            <p class="text-stroke">02</p>
+                            Более 400 кв.м для проведения веселого мероприятия с праздничным застольем и развлекательной авторской программой
+                        </div>
 					</div>
 				</div>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-3.svg" alt=""></div>
-						<div class="like-text">Возможность наблюдать за игрой участников с помощью live-трансляции</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-3.jpg" alt=""></div>
+                        <div class="like-text">
+                            <p class="text-stroke">03</p>
+                            Фантастический галактический дизайн с любимыми детскими супергероями
+                        </div>
 					</div>
 				</div>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-4.svg" alt=""></div>
-						<div class="like-text">Гарантия полной безопасности — приключения под контролем профессиональных инструкторов</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-4.jpg" alt=""></div>
+                        <div class="like-text">
+                            <p class="text-stroke">04</p>
+                            Задания и аттракционы,<br> разработанные<br> профессиональными психологами, педагогами и игротехниками
+                        </div>
 					</div>
 				</div>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-5.svg" alt=""></div>
-						<div class="like-text">Проведение незабываемых праздников с чаепитием и авторской программой (под ключ)</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-5.jpg" alt=""></div>
+                        <div class="like-text">
+                            <p class="text-stroke">05</p>
+                            Гарантия полной безопасности – приключения под контролем профессиональных инструкторов
+                        </div>
 					</div>
 				</div>
 				<div>
 					<div class="like-item">
-						<div class="like-icon"><img src="images/like-6.svg" alt=""></div>
-						<div class="like-text">А главное — вы и ваши дети получите нереальный заряд положительных эмоций на долгое время!</div>
+						<div class="like-icon"><img src="images/holding-a-holiday-6.jpg" alt=""></div>
+                        <div class="like-text">
+                            <p class="text-stroke">06</p>
+                            Возможность для<br> сопровождающих наблюдать за игрой по телевизору в отдельном зале на удобных диванах
+                        </div>
 					</div>
 				</div>
 
@@ -176,35 +199,35 @@
 			</ul>
 		</div>
 	</div>
-	<div class="section-white section-padding">
-		<div class="uk-container container-780"  data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
-			<div class="uk-h2 uk-margin-large-bottom text-center-small">
-				Центр «Галактика приключений»
-				предлагает игры для участников от 6 лет.
-				Команды пройдут испытания и выполнят задания "Во благо Галактики" 
-			</div>
-			<div class="list-numbers-padding uk-h3 uk-margin-bottom">Проведение праздника в нашем центре это:</div>
-			<ul class="list-numbers uk-grid uk-grid-width-medium-1-2" data-uk-grid-margin data-uk-grid-match>
-				<li>
-					<div class="list-number">01.</div>
-					<div class="list-number-text">Космические приключения с испытаниями, головоломками и заданиями</div>
-				</li>
-				<li>
-					<div class="list-number">02.</div>
-					<div class="list-number-text">Развлекательные программы от наших ведущих получение наград и званий «Хранители Галактики»</div>
-				</li>
-				<li>
-					<div class="list-number">03.</div>
-					<div class="list-number-text">Зал для торжеств и организация чаепития</div>
-				</li>
-				<li>
-					<div class="list-number">04.</div>
-					<div class="list-number-text">Live-трансляция прохождения игр-приключений</div>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="section-panel">
+<!--	<div class="section-white section-padding">-->
+<!--		<div class="uk-container container-780"  data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">-->
+<!--			<div class="uk-h2 uk-margin-large-bottom text-center-small">-->
+<!--				Центр «Галактика приключений»-->
+<!--				предлагает игры для участников от 6 лет.-->
+<!--				Команды пройдут испытания и выполнят задания "Во благо Галактики" -->
+<!--			</div>-->
+<!--			<div class="list-numbers-padding uk-h3 uk-margin-bottom">Проведение праздника в нашем центре это:</div>-->
+<!--			<ul class="list-numbers uk-grid uk-grid-width-medium-1-2" data-uk-grid-margin data-uk-grid-match>-->
+<!--				<li>-->
+<!--					<div class="list-number">01.</div>-->
+<!--					<div class="list-number-text">Космические приключения с испытаниями, головоломками и заданиями</div>-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					<div class="list-number">02.</div>-->
+<!--					<div class="list-number-text">Развлекательные программы от наших ведущих получение наград и званий «Хранители Галактики»</div>-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					<div class="list-number">03.</div>-->
+<!--					<div class="list-number-text">Зал для торжеств и организация чаепития</div>-->
+<!--				</li>-->
+<!--				<li>-->
+<!--					<div class="list-number">04.</div>-->
+<!--					<div class="list-number-text">Live-трансляция прохождения игр-приключений</div>-->
+<!--				</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--	</div>-->
+	<div class="section-panel adventure-game">
 		<div class="uk-container uk-container-center container-780"  data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
 			<div class="panel-white">
 				<div class="uk-h3 uk-text-center uk-margin-large-bottom">
@@ -212,12 +235,12 @@
 				</div>
 				<p class="uk-margin-large-bottom">У нас Вы можете заказать не только веселый и удивительный праздник под ключ, но и просто посетить наш центр дружной командой и пройти увлекательные испытания в выходные, будние дни и даже в каникулы. А мы поможем провести это время максимально ярко, насыщенно и незабываемо!</p>
 				<div class="uk-text-center">
-					<a class="uk-button button-56">Подробнее об услугах</a>
+					<a class="uk-button button-56 uk-button__fix">Подробнее об услугах</a>
 				</div>
-				<div class="panel-white-icon panel-white-icon_1"><img src="images/panel-white-1.svg" alt=""></div>
-				<div class="panel-white-icon panel-white-icon_2"><img src="images/panel-white-2.svg" alt=""></div>
-				<div class="panel-white-icon panel-white-icon_3"><img src="images/panel-white-3.svg" alt=""></div>
-				<div class="panel-white-icon panel-white-icon_4"><img src="images/panel-white-4.svg" alt=""></div>
+<!--				<div class="panel-white-icon panel-white-icon_1"><img src="images/panel-white-1.svg" alt=""></div>-->
+				<div class="panel-white-icon panel-white-icon_2"><img src="images/shooting-star.svg" alt=""></div>
+<!--				<div class="panel-white-icon panel-white-icon_3"><img src="images/panel-white-3.svg" alt=""></div>-->
+<!--				<div class="panel-white-icon panel-white-icon_4"><img src="images/panel-white-4.svg" alt=""></div>-->
 			</div>
 		</div>
 	</div>
@@ -238,7 +261,7 @@
 					ТЦ "Новая эра", этаж 5</p>
 					<p class="text-26"><strong>8 (831) 414-63-33</strong></p>
 					<p class="uk-text-small uk-margin-large-bottom">Мы работаем ежедневно с 10:00 до 20:30</p>
-					<p><a href="" class="uk-button button-56">Оставить заявку</a></p>
+					<p><a href="" class="uk-button button-56 uk-button__fix">Оставить заявку</a></p>
 				</div>
 			</div>
 		</div>
--- services.php
+++ services.php
@@ -19,24 +19,24 @@
 			<div class="uk-slidenav-position" data-uk-slideshow>
 				<ul class="uk-slideshow">
 					<li>
-						<div class="slideshow-promo-slide">
-							<div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
-							<div class="slide-content">
-								<div class="slide-title">Заголовок акции, сезонного уникального предложения</div>
-								<div class="slide-text">Дата проведения акции 1- 9 января</div>
-								<a class="uk-button slide-button">Подробнее</a>
-							</div>
-						</div>
+                        <div class="slideshow-promo-slide">
+                            <div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
+                            <div class="slide-content">
+                                <div class="slide-title text-stroke">Скидка 33%</div>
+                                <div class="slide-text">в будние дни до 15:00</div>
+                                <a class="uk-button slide-button">Подробнее</a>
+                            </div>
+                        </div>
 					</li>
 					<li>
-						<div class="slideshow-promo-slide">
-							<div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
-							<div class="slide-content">
-								<div class="slide-title">Заголовок акции, сезонного уникального предложения</div>
-								<div class="slide-text">Дата проведения акции 1- 9 января</div>
-								<a class="uk-button slide-button">Подробнее</a>
-							</div>
-						</div>
+                        <div class="slideshow-promo-slide">
+                            <div class="slide-image"><img src="images/slide-image.jpg" alt=""></div>
+                            <div class="slide-content">
+                                <div class="slide-title text-stroke">Скидка 33%</div>
+                                <div class="slide-text">в будние дни до 15:00</div>
+                                <a class="uk-button slide-button">Подробнее</a>
+                            </div>
+                        </div>
 					</li>
 				</ul>
 				<a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
@@ -51,8 +51,8 @@
 	</div>
 </div>
 
-<div class="section-white section-padding section-icons">
-	<div class="uk-container uk-container-center container-940" data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
+<div class="section-white section-padding section-icons service-fix">
+	<div class="uk-container uk-container-center container-940 uk-margin-bottom" data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
 		<div class="uk-h1 text-stroke text-stroke_blue uk-margin-large-bottom uk-text-center">
 			Наши услуги
 		</div>
@@ -61,17 +61,17 @@
 			Проведите незабываемый праздник
 			в нашем развлекательном центре!
 		</div>
-		<div class="services-blocks uk-grid uk-grid-small uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-1-1" data-uk-grid-match="{target:'.service-teaser'}" data-uk-grid-margin>
+		<div class="services-blocks uk-grid uk-grid-small uk-grid-width-large-1-3 uk-grid-width-medium-1-2 uk-grid-width-1-1 uk-margin-large-bottom" data-uk-grid-match="{target:'.service-teaser'}" data-uk-grid-margin>
 			<div>
 				<div class="service-teaser service-birthday">
-					<div class="service-icon"><img src="images/icon-birthday.svg" alt=""></div>
+<!--					<div class="service-icon"><img src="images/icon-birthday.svg" alt=""></div>-->
 					<div class="service-name">Дни рождения</div>
 					<a href="" class="uk-position-cover"></a>
 				</div>
 			</div>
 			<div>
 				<div class="service-teaser service-group">
-					<div class="service-icon"><img src="images/icon-group.svg" alt=""></div>
+<!--					<div class="service-icon"><img src="images/icon-group.svg" alt=""></div>-->
 					<div class="service-name">Групповые
 					посещения</div>
 					<a href="" class="uk-position-cover"></a>
@@ -80,7 +80,7 @@
 			</div>
 			<div>
 				<div class="service-teaser service-individual">
-					<div class="service-icon"><img src="images/icon-individual.svg" alt=""></div>
+<!--					<div class="service-icon"><img src="images/icon-individual.svg" alt=""></div>-->
 					<div class="service-name">Индивидуальное посещение</div>
 					<a href="" class="uk-position-cover"></a>
 
@@ -91,6 +91,33 @@
 			<a href="" class="uk-button uk-button-large">Оставить заявку</a>
 		</div>
 	</div>
+
+    <div class="uk-container uk-container-center container-940" data-uk-scrollspy="{cls:'uk-animation-slide-bottom'}">
+        <div class="uk-h1 service-add-h1 text-stroke text-stroke_blue uk-text-center">
+            Дополнительно
+        </div>
+        <div class="uk-grid">
+            <div class="uk-width-1-1 uk-width-large-1-2 service-add-items">
+                <img src="images/digital-camera.svg" alt="digital-camera">
+                <div class="service-add-cont">
+                    <div class="service-add-title">Фотограф</div>
+                    <p class="service-add-txt">
+                        Если Вы хотите оставить после праздника не только яркие воспоминания, но и красочные фотографии, наш
+                        фотограф с удовольствием вам в этом поможет! Просто при бронировании даты сообщите, что вам нужны
+                        услуги фотографа.
+                    </p>
+                </div>
+            </div>
+            <div class="uk-width-1-1 uk-width-large-1-2 service-add-items">
+                <img src="images/balloons.svg" alt="balloons">
+                <div class="service-add-cont">
+                    <div class="service-add-title">Воздушные шары</div>
+                    <p class="service-add-txt">Если Вы хотите красиво оформить Ваше мероприятие, мы надуем гелиевые воздушные шары.
+                        Заказать эту услугу можно заранее при бронировании праздника.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <? include 'footer.php'; ?>
